ef4c377d012ddb71d791494af597270066ee2f92
Ricky <56303584+runningindos@users.noreply.github.com>
2024-03-29 00:41:04 +0000

    Set secret keys, added api key and function

--- petrolwars_project/users/forms.py
+++ petrolwars_project/users/forms.py
@@ -1,9 +1,9 @@
 from django import forms
 from django.contrib.auth import get_user_model
 from django.contrib.auth.forms import UserCreationForm
-
 from crispy_forms.helper import FormHelper
 from crispy_forms.layout import Submit
+from .models import Profile
 
 User = get_user_model()
 
@@ -19,3 +19,15 @@ class UserRegisterForm(UserCreationForm):
     class Meta:
         model = User
         fields = ['username', 'email', 'password1', 'password2']
+
+class UserUpdateForm(forms.ModelForm):
+    email = forms.EmailField()
+
+    class Meta:
+        model = User
+        fields = ['username', 'email']
+
+class ProfileUpdateForm(forms.ModelForm):
+    class Meta:
+        model = Profile
+        fields = ['image']
--- petrolwars_project/users/models.py
+++ petrolwars_project/users/models.py
@@ -1,6 +1,5 @@
 from django.db import models
 from django.contrib.auth.models import User
-from django_resized import ResizedImageField
 from PIL import Image
 
 # Create your models here.
@@ -10,3 +9,13 @@ class Profile(models.Model):
 
     def __str__(self):
         return f'{self.user.username} Profile'
+
+    def save(self, *args, **kwargs):
+        super().save(*args, **kwargs)
+
+        img = Image.open(self.image.path)
+
+        if img.height > 200 or img.width > 200:
+            output_size = (200, 200)
+            img.thumbnail(output_size)
+            img.save(self.image.path)
